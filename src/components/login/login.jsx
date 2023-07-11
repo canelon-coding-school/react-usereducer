6e34541f9a189bed18830ca1ac3b509ace432a93
Nicolas Danelon <nicolasdanelon@gmail.com>
2023-07-12 00:12:10 +0200

    login use state

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,19 +1,55 @@
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import './login.css';
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:9001/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({ username, password })
+    });
+
+    const responseJson = await response.json();
+
+    if (responseJson.status === 'success') {
+      setUsername('');
+      setPassword('');
+      return navigate('/dashboard');
+    } else {
+      alert('error');
+    }
+  };
 
   return (
     <div className="login">
       <h1>Login</h1>
-      <form method="post" onSubmit={(e) => e.preventDefault()}>
-        <input type="text" name="u" placeholder="Username" />
-        <input type="password" name="p" placeholder="Password" />
+      <form method="post" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
         <button
           type="submit"
           className="btn btn-primary btn-block btn-large"
-          onClick={() => { navigate('/dashboard') }}
+          disabled={!(username.length > 1 && password.length > 1)}
         >
           Let me in
         </button>
